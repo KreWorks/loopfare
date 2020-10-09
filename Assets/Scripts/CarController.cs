1c6bfa223a9702fa2061d4eab5b0317c43e44b9d
Császár-Kozma Réka <rekuci86@gmail.com>
2020-10-09 20:08:00 +0200

    Movement implementation

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	public float carSpeed;
+	public float laneWitdh = 1.0f;
+
+	Rigidbody rb; 
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		rb.velocity = this.transform.forward * carSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		rb.velocity = this.transform.forward * carSpeed;
+		
+		if(this.transform.position.x > -laneWitdh && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)))
+		{
+			this.transform.position += laneWitdh * Vector3.left;
+		}
+
+		if(this.transform.position.x < laneWitdh && (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)))
+		{
+			this.transform.position -= laneWitdh * Vector3.left;
+		}
+	}
+}
